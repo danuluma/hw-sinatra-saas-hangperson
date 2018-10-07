1208b9714895893ab79f18caaaa73d8381fc3d2c
Dan Uluma <danuluma@gmail.com>
2018-10-08 02:28:56 +0300

    part1 tdd- created game logic

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -15,7 +15,42 @@ class HangpersonGame
     @wrong_guesses = ''
   end
 
+  def guess(xter)
+    if xter == nil || !(xter.class == String && xter =~ /^[A-z]$/i)
+      raise ArgumentError
+    end
+    xter.downcase!
+    if @guesses.include?(xter) || @wrong_guesses.include?(xter)
+      return false
+    end
+    if @word.include?(xter)
+      @guesses << xter
+    else
+      @wrong_guesses << xter
+    end
+  end
 
+  def word_with_guesses
+    disp = ''
+    @word.split('').each do |xter|
+      if @guesses.include?(xter)
+        disp << xter
+      else
+        disp << '-'
+      end
+    end
+    return disp
+  end
+
+  def check_win_or_lose
+    if word_with_guesses.downcase == @word.downcase
+      return :win
+    elsif @wrong_guesses.length >= 7
+      return :lose
+    else
+      return :play
+    end
+  end
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
